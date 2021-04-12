5c251547cca3ace6b71795e17b67628073c84ba9
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-04-12 16:59:49 -0700

    Added urls/new page with form to POST new longURL

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Set ejs as view engine
 app.set("view engine", "ejs");
@@ -10,6 +13,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
   "SVB-Twitch": "https://www.twitch.tv/owsvb"
 };
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -24,6 +28,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the server console
+  res.send("Ok"); // Respond with 'Ok' (we will do this replacement)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
@@ -32,6 +45,10 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+// app.get("/urls/:id", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
