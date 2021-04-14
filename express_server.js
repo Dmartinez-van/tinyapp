d28251386755e205e3ecc464f748d3bfac057e1c
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-04-14 14:54:11 -0700

    Working on getting urls to show only to certain users

--- express_server.js
+++ express_server.js
@@ -23,9 +23,18 @@ app.set("view engine", "ejs");
 //
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-  "SVB-Twitch": "https://www.twitch.tv/owsvb"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "testUser"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "testUser"
+  },
+  "SVB-Twitch": {
+    longURL: "https://www.twitch.tv/owsvb",
+    userID: "testUser"
+  }
 };
 
 const users = {
@@ -42,6 +51,37 @@ const users = {
 };
 
 //
+// Function(s)
+//
+
+const generateRandomString = function() {
+  let randString = (Math.random() + 1).toString(36).substring(2, 8);
+  return randString;
+};
+
+// return true for duplicate, false for no duplicate
+const checkForDuplicate = function(newEmail) {
+  // console.log("check for ", newEmail, "in users databse------");
+  for (const user in users) {
+    // console.log("Checking...", user);
+    // console.log("for...", newEmail);
+    if (users[user].email === newEmail) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// return urls for the specified id
+const urlsForUser = function(id) {
+  for (const user in users) {
+    if (users[user] === id) {
+      return user;
+    }
+  }
+};
+
+//
 // Main Pages
 //
 
@@ -50,13 +90,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, users: users, userid: req.cookies['userid']};
-  console.log(users);
+  let personalURL = {};
+  
+  const templateVars = { urls: urlDatabase, users: users, userid: req.cookies['userid'] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = { urls: urlDatabase, users: users, userid: req.cookies['userid'] };
+  const templateVars = { urls: urlDatabase.longURL, users: users, userid: req.cookies['userid'] };
   res.render("register", templateVars);
 });
 
@@ -64,14 +105,20 @@ app.get("/register", (req, res) => {
 // User must pick one of the other in order to create new shortURL
 // Apply this to Edit featuer as well
 app.get("/urls/new", (req, res) => {
+  console.log("req.cookies", req.cookies);
+  // console.log("req.cookie", req.cookie);
   const templateVars = { users: users, userid: req.cookies['userid'] };
-  res.render("urls_new", templateVars);
+  if (req.cookies['userid']) {
+    res.render("urls_new", templateVars);
+    return;
+  }
+  res.redirect("/login");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   // req.params.shortURL points to the ' :/shortURL ' in the url. Which is whatever we generated from the app.post generateRandomString() block.
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
+  let longURL = urlDatabase[shortURL].longURL;
   // Set templateVars from the url provided from get method and the database
   const templateVars = { shortURL, longURL, users: users, userid: req.cookies['userid'] };
   res.render("urls_show", templateVars);
@@ -79,7 +126,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
+  let longURL = urlDatabase[shortURL].longURL;
   // USER MUST TYPE IN HTTP:// in order for this to work.
   res.redirect(longURL);
 });
@@ -125,17 +172,16 @@ app.post("/logout", (req, res) => {
 //
 
 app.post("/register", (req, res) => {
-  
   if (!req.body.email || !req.body.password) {
     res.status(400);
     res.send(`Error ${res.statusCode}\nPlease ensure the email and password fields are filled out.`);
   }
-  
   if (checkForDuplicate(req.body.email)) {
     res.status(400);
     res.send(`Error ${res.statusCode}\nThis email has already been registered.`);
     return;
   }
+
   let newUser = {
     id: generateRandomString(),
     email: req.body.email,
@@ -144,7 +190,6 @@ app.post("/register", (req, res) => {
 
   users[newUser.id] = newUser;
   res.cookie('userid', newUser.id);
-
   res.redirect("/urls");
 });
 
@@ -152,14 +197,16 @@ app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   // Grab whatever user enters into the form and set it as the value to the generated short URL key
   // body-parser is a middleware which is populating the req.body object
-  urlDatabase[shortURL] = req.body.longURL;
+  urlDatabase[shortURL] = {};
+  urlDatabase[shortURL].longURL = req.body.longURL;
+  urlDatabase[shortURL].userID = req.cookies.userid;
+  
   res.redirect(`/urls/${shortURL}`);
 });
 
-
 app.post("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  urlDatabase[shortURL] = req.body.longURL;
+  urlDatabase[shortURL].longURL = req.body.longURL;
   res.redirect('/urls');
 });
 
@@ -181,27 +228,6 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//
-// Function(s)
-//
-
-const generateRandomString = function() {
-  let randString = (Math.random() + 1).toString(36).substring(2, 8);
-  return randString;
-};
-
-// return true for duplicate, false for no duplicate
-const checkForDuplicate = function(newEmail) {
-  // console.log("check for ", newEmail, "in users databse------");
-  for (const user in users) {
-    // console.log("Checking...", user);
-    // console.log("for...", newEmail);
-    if (users[user].email === newEmail) {
-      return true;
-    }
-  }
-  return false;
-};
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
