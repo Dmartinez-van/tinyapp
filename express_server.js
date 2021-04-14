fd95b40e73d37d8c94f331ecf61bf05289952a81
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-04-14 11:26:20 -0700

    Formatting and added return to app.post('/register')

--- express_server.js
+++ express_server.js
@@ -4,11 +4,23 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
 
+
+//
+// Middleware
+//
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+//
 // Set ejs as view engine
+//
+
 app.set("view engine", "ejs");
+
+//
+// Databases
+//
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -43,70 +55,9 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
-  // Grab whatever user enters into the form and set it as the value to the generated short URL key
-  // body-parser is a middleware which is populating the req.body object
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);
-});
-
 app.get("/register", (req, res) => {
   const templateVars = { urls: urlDatabase, users: users, userid: req.cookies['userid'] };
   res.render("register", templateVars);
-});
-
-//
-// Header stuff
-//
-
-app.post("/login", (req, res) => {
-  for (const userID in users) {
-    if (users[userID].email && users[userID].password === req.body.password) {
-      res.cookie("userid", userID);
-    }
-  }
-  res.redirect("/urls");
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie('userid');
-  res.redirect("/urls");
-});
-
-//
-// Create (Update)
-//
-
-app.post("/register", (req, res) => {
-  
-  if (!req.body.email || !req.body.password) {
-    res.status(400);
-    res.send(`Error ${res.statusCode}\nPlease ensure the email and password fields are filled out.`);
-  }
-  
-  if (checkForDuplicate(req.body.email)) {
-    res.status(400);
-    res.send(`Error ${res.statusCode}\nThis email has already been registered.`);
-  } else {
-    let newUser = {
-      id: generateRandomString(),
-      email: req.body.email,
-      password: req.body.password
-    };
-  
-    users[newUser.id] = newUser;
-    
-    res.cookie('userid', newUser.id);
-  
-    res.redirect("/urls");
-  }
-});
-
-app.post("/urls/:shortURL", (req, res) => {
-  let shortURL = req.params.shortURL;
-  urlDatabase[shortURL] = req.body.longURL;
-  res.redirect('/urls');
 });
 
 // Add a toggle option to select either http:// or https://
@@ -134,6 +85,68 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 //
+// Login & Logout
+//
+
+app.post("/login", (req, res) => {
+  for (const userID in users) {
+    if (users[userID].email && users[userID].password === req.body.password) {
+      res.cookie("userid", userID);
+    }
+  }
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('userid');
+  res.redirect("/urls");
+});
+
+//
+// Register & Create URLs
+//
+
+app.post("/register", (req, res) => {
+  
+  if (!req.body.email || !req.body.password) {
+    res.status(400);
+    res.send(`Error ${res.statusCode}\nPlease ensure the email and password fields are filled out.`);
+  }
+  
+  if (checkForDuplicate(req.body.email)) {
+    res.status(400);
+    res.send(`Error ${res.statusCode}\nThis email has already been registered.`);
+    return;
+  }
+  let newUser = {
+    id: generateRandomString(),
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  users[newUser.id] = newUser;
+  res.cookie('userid', newUser.id);
+
+  res.redirect("/urls");
+});
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  // Grab whatever user enters into the form and set it as the value to the generated short URL key
+  // body-parser is a middleware which is populating the req.body object
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+
+app.post("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect('/urls');
+});
+
+
+//
 // Delete
 //
 
@@ -143,7 +156,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 //
-// Read
+// Read JSON
 //
 
 app.get("/urls.json", (req, res) => {
